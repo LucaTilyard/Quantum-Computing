aac48638ce15c05f505c1e9837600d21801cb642
Luca Tilyard <96886916+chu106@users.noreply.github.com>
2025-03-07 14:56:49 +0000

    cleaned SDC

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Quantum-Computing
 This repository contains examples of quantum systems built using IBM's Qiskit quantum computing SDK.
 
----
 
 ## Superdense Coding
 
 Superdesnse coding is a method of encoding two classical bits of information into a single qubit. This is done by entangling two qubits and then applying a series of quantum gates to the qubit that contains the information. The qubit is then sent to the receiver who can decode the information by applying a series of quantum gates to the qubit.
 In this example "Alice" is the sender and "Bob" is the receiver. 
 
---- 
+#### Example of 11 Superdense Coding
+![Example of 11 Superdense Coding](src/11.png)
+
--- SuperdenseCoding.py
+++ SuperdenseCoding.py
@@ -1,55 +1,58 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit.primitives import StatevectorSampler
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Generate bell state
+
+# Generate bell state.
 alice = QuantumRegister(1, 'alice')
 bob = QuantumRegister(1, 'bob')
 alpha = ClassicalRegister(1, 'alpha')
 beta = ClassicalRegister(1, 'beta')
 
-
+# Construst the circuit.
 qc = QuantumCircuit(alice, bob, alpha, beta)
 qc.h(0)
 qc.cx(0,1)
 qc.barrier()
-# Alice is trying to send Bob (1,1)
+
+# Add gates depending on the message.
+# 00 -> Do nothing
+# 10 -> Apply Z gate
+# 01 -> Apply X gate
+# 11 -> Apply Z and X gate
 qc.z(0)
-#qc.x(0)
+qc.x(0)
+
 qc.barrier()
-# Bob receives the qubits
+
+# Send Qubits to Bob.
 qc.cx(0,1)
 qc.h(0)
 qc.barrier()
 qc.measure(alice, alpha)
 qc.measure(bob, beta)
 
+# Draw and optionally save image of the circuit.
 qc.draw(output='mpl')
+#plt.savefig("11.png")
 plt.show()
+
 
 # Instantiate a new statevector simulation based sampler object.
 sampler = StatevectorSampler()
 
-# Start a job that will return shots for all 100 parameter value sets.
-job = sampler.run([qc], shots=256)
+# Start a job.
+job = sampler.run([qc], shots=1)
 
 # Extract the result for the 0th pub (this example only has one pub).
 result = job.result()[0]
 
-# For small registers where it is anticipated to have many counts
-# associated with the same bitstrings, we can turn the data from,
-# for example, the 22nd sweep index into a dictionary of counts.
-counts = result.data.alpha.get_counts(0)
 
-# Or, convert into a list of bitstrings that preserve shot order.
-bitstrings = result.data.alpha.get_bitstrings(0)
+# Convert into a list of bitstrings.
+bitstring = result.data.alpha.get_bitstrings(0)
+print(f"First Bit: {bitstring[0]}")
 
-print(f"First Bit: {bitstrings[0]}")
+# Convert into a list of bitstrings.
+bitstring2 = result.data.beta.get_bitstrings(0)
+print(f"Second Bit: {bitstring2[0]}")
 
-counts2 = result.data.beta.get_counts(0)
-
-# Or, convert into a list of bitstrings that preserve shot order.
-bitstrings2 = result.data.beta.get_bitstrings(0)
-
-print(f"Second Bit: {bitstrings2[0]}")
